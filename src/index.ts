03f589c6f864a3eb7944617e009bdcba956bcd07
Joel cues <libra-marriaga@hotmail.com>
2022-11-08 12:04:54 -0500

    seccion namespaces terminada

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import { Hero } from "./classes/Hero";
+// import { Hero as SuperHero, Hero2 } from "./classes/Hero";
+// import * as HeroClases from "./classes/Hero";
+// import powers from "./data/powers";
+
+// const Hero = 123
+
+console.log('Hola Mundo!!!');
+// const iroman = new SuperHero("Iroman", 1, 55)
+const iroman = new Hero("Iroman", 1, 55)
+
+
+console.log(iroman.power);
+
+// console.log( powers )
+
+
+
+
+
