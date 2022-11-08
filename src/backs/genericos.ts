4d116278a1fb074f4cc1a6c2009cf51a1164542e
Joel cues <libra-marriaga@hotmail.com>
2022-11-08 17:47:13 -0500

    seccion de genericos terminada

---
+++ src/backs/genericos.ts
@@ -0,0 +1,24 @@
+import { printObject, genericFunction, genericFunctionArrow } from "../generics/generics";
+import { Villain, Hero } from "../interfaces";
+// import { Hero } from "./interfaces/hero";
+// import { Villain } from "./interfaces/villain";
+
+
+// printObject( 123 )
+// printObject( new Date() )
+// printObject( { a:1, b:2, c:3 } )
+// printObject( [1,2,3,4,5] )
+// printObject( "Hola Mundo" )
+// const name : string = "Joel"
+// console.log( genericFunction(3.141618).toFixed(2) )
+// console.log( genericFunctionArrow(name).toUpperCase())
+// console.log( genericFunction( new Date() ).getDate() )
+
+
+const deadpool = {
+    name: "Deadpool",
+    realName: "Wade Winston Wilson",
+    dangerLevel: 130
+}
+
+console.log( genericFunctionArrow<Villain>(deadpool).dangerLevel )
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,7 @@
-import { Hero } from "./classes/Hero";
-// import { Hero as SuperHero, Hero2 } from "./classes/Hero";
-// import * as HeroClases from "./classes/Hero";
-// import powers from "./data/powers";
-
-// const Hero = 123
-
-console.log('Hola Mundo!!!');
-// const iroman = new SuperHero("Iroman", 1, 55)
-const iroman = new Hero("Iroman", 1, 55)
+import { getPokemon } from "./generics/get-pokemons";
 
 
-console.log(iroman.power);
-
-// console.log( powers )
-
-
-
-
-
+getPokemon(4)
+    .then( pokemon => console.log(pokemon.sprites) )
+    .catch( error => console.error(error) )
+    .finally( ()=> console.log("Fin del pokemon") ) 
